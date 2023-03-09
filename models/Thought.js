9408d42639189b6c0a8eed4dea471390f4ee9030
sanchezju <sjuan634@yahoo.com>
2023-03-08 23:05:21 -0800

    add dateFormat module

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,7 @@
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
+
 
 const thoughtSchema = new Schema(
   {
